e0d8a8c17788f7236984611b8f7f433d5338d3f2
mooreTomas <tommymoore@live.ie>
2024-11-15 06:46:44 -0500

    schema fix

--- laptop-schema.sql
+++ laptop-schema.sql
@@ -1,11 +1,11 @@
 DROP TABLE laptop;
 
 CREATE TABLE laptop (
-    int id PRIMARY KEY NOT NULL,
+    id int PRIMARY KEY NOT NULL,
     product VARCHAR(512),
     brand	VARCHAR(512),
     CPU	VARCHAR(512),
     memory VARCHAR(512),
-    price	NUMBER
+    price NUMBER(10,2)
 );
 
